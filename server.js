85d93da421d9524356c7f10bf853fdbd42a353d3
sri sidhu <srisidhu@EZIO.COM>
2025-07-06 23:13:36 +0530

    uri changes

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const connectDB = require('./config/db');
-const expenseRoutes = require('./routes/expense');
-const csvExcel = require('./routes/csvExcel');
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const expenseRoutes = require("./routes/expense");
+const csvExcel = require("./routes/csvExcel");
 
 dotenv.config();
 connectDB();
@@ -11,18 +11,21 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "https://simple-expense-tracker-fe.vercel.app " || "http://localhost:5173" || "http://localhost:5000",
+    origin:
+      "https://simple-expense-tracker-fe.vercel.app " ||
+      "http://localhost:5173" ||
+      "http://localhost:5000",
     credentials: true,
   })
 );
 app.use(express.json());
-app.use('/downloads', express.static('downloads'));
-app.use('/api/expenses', csvExcel);
-app.use('/api/expenses', expenseRoutes);
+app.use("/downloads", express.static("downloads"));
+app.use("/api/expenses", csvExcel);
+app.use("/api/expenses", expenseRoutes);
 
-app.get('/', (req, res) => {
-  console.log('Hello API hit');
-  res.json({ message: 'Hello, Node!' });
+app.get("/", (req, res) => {
+  console.log("Hello API hit");
+  res.json({ message: "Hello, Node!" });
 });
 
 const PORT = process.env.PORT || 5000;
