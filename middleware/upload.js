c54341e1c74d1c9521913870c8d7c20e24f4dfe8
sri sidhu <srisidhu@EZIO.COM>
2025-07-06 21:29:50 +0530

    bug fixed

--- middleware/upload.js
+++ middleware/upload.js
@@ -1,11 +1,10 @@
-// middleware/upload.js
+
 const multer = require("multer");
 const path = require("path");
 
-// Storage config
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Make sure this folder exists
+    cb(null, "uploads/"); 
   },
   filename: (req, file, cb) => {
     const uniqueName = `${Date.now()}-${file.originalname}`;
@@ -13,7 +12,6 @@ const storage = multer.diskStorage({
   },
 });
 
-// CSV file filter
 const fileFilter = (req, file, cb) => {
   const ext = path.extname(file.originalname).toLowerCase();
   if (ext === ".csv") {
@@ -26,7 +24,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   fileFilter,
-  limits: { fileSize: 1 * 1024 * 1024 }, // Optional: max 1MB
+  limits: { fileSize: 1 * 1024 * 1024 }, 
 });
 
 module.exports = upload;
--- server.js
+++ server.js
@@ -9,7 +9,12 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://simple-expense-tracker-fe.vercel.app",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use('/downloads', express.static('downloads'));
 app.use('/api/expenses', csvExcel);
