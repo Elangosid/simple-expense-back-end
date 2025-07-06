fa451d71484c6501aad17d5badfa5f2df5ca992f
sri sidhu <srisidhu@EZIO.COM>
2025-07-06 23:34:04 +0530

    cors fixes

--- server.js
+++ server.js
@@ -9,15 +9,27 @@ dotenv.config();
 connectDB();
 
 const app = express();
+const allowedOrigins = [
+  "https://simple-expense-tracker-fe.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:5000",
+];
+
 app.use(
   cors({
-    origin:
-      "https://simple-expense-tracker-fe.vercel.app " ||
-      "http://localhost:5173" ||
-      "http://localhost:5000",
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like mobile apps, curl, postman)
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      } else {
+        return callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use("/downloads", express.static("downloads"));
 app.use("/api/expenses", csvExcel);
