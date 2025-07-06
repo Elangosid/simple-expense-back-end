f8b8726abab1d49d164f8cad48a3f3d8a5dfd1a5
sri sidhu <srisidhu@EZIO.COM>
2025-07-06 21:38:29 +0530

    cors added

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ connectDB();
 const app = express();
 app.use(
   cors({
-    origin: "https://simple-expense-tracker-fe.vercel.app " || "http://localhost:5173",
+    origin: "https://simple-expense-tracker-fe.vercel.app " || "http://localhost:5173" || "http://localhost:5000",
     credentials: true,
   })
 );
